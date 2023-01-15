3acc1cc921f81dce053941adae095831b1578e53
TheTimickRus <andrey.timofeev.and37@gmail.com>
2023-01-15 16:12:52 +0300

    Исправления и улучшения

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <MainPage/>
+  <MainPage class="overflow-hidden"/>
 </template>
 
 
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,29 +1,29 @@
 <template>
-  <div class="min-h-screen h-1 overflow-hidden relative">
+  <div class="h-mobile sm:min-h-screen  flex flex-col">
     <div class="absolute h-full w-full opacity-80 bg-main"></div>
     <img
         src="../assets/img/main-background.jpg"
         height="1080"
         width="1920"
-        class="absolute h-full w-full -z-10  object-cover"
+        class="absolute h-full w-full -z-10 object-cover"
     />
-    <header class="flex px-10 sm:px-16 relative justify-center sm:justify-start h-16 sm:h-24 items-center">
+    <header class="sticky top-0 flex sm:px-16 relative justify-center sm:justify-start h-16 sm:h-24 items-center">
       <div class="absolute left-0 h-full w-full bg-main drop-shadow-black opacity-50"></div>
       <div class="absolute left-0 h-full w-full bg-main blur-xl opacity-50"></div>
       <div class="flex items-center gap-5 z-10">
         <AppIcon name="logo" class="w-10 h-10 sm:w-16 sm:h-16 logo"/>
-        <span class=" text-2xl sm:text-4xl text-main font-bold">TheTimickRus</span>
+        <span class="text-2xl sm:text-4xl text-main font-bold">TheTimickRus</span>
       </div>
     </header>
-    <div class="container h-full mx-auto">
-      <div class="flex items-center h-full justify-center">
-        <div class="flex flex-col gap-12">
+    <div class="container flex-1 flex justify-center mx-auto">
+      <div class="flex items-center justify-center">
+        <div class="flex flex-col mt-[60px] sm:mt-0 gap-12">
           <div class="flex flex-col gap-3">
             <div class="flex justify-center gap-3">
               <div class="relative drop-shadow-main rounded-full bg-main opacity-50 ">
                 <img
-                  class="z-100 object-cover h-[140px] rounded-full w-[140px] sm:w-[200px] sm:h-[200px]"
-                  src="../assets/img/developer-photo.jpg"
+                    class="z-100 object-cover h-[140px] rounded-full w-[140px] sm:w-[200px] sm:h-[200px]"
+                    src="../assets/img/developer-photo.jpg"
                 />
               </div>
             </div>
@@ -36,46 +36,46 @@
               </div>
             </div>
           </div>
-          <div class="flex relative rounded-xl items-center justify-around gap-4 sm:gap-12 p-5">
+          <div class="flex relative rounded-2xl items-center gap-4 sm:gap-12 p-4 sm:p-5">
             <div class="absolute rounded-xl left-0 h-full w-full bg-main drop-shadow-main opacity-50"></div>
             <div class="absolute rounded-xl left-0 h-full w-full bg-main blur-xl opacity-50"></div>
             <a href="https://www.instagram.com/thetimickrus/" target="_blank">
-              <div class="social-list">
+              <div class="social-circle">
                 <AppIcon
-                  name="instagram"
-                  class="social-icon"
+                    name="instagram"
+                    class="social-icon"
                 />
               </div>
             </a>
             <a href="https://vk.com/thetimickrus" target="_blank">
-              <div class="social-list">
+              <div class="social-circle">
                 <AppIcon
-                  name="vk"
-                  class="social-icon"
+                    name="vk"
+                    class="social-icon"
                 />
               </div>
             </a>
             <a href="https://github.com/TheTimickRus" target="_blank">
-              <div class="social-list">
+              <div class="social-circle">
                 <AppIcon
-                  name="github"
-                  class="social-icon"
+                    name="github"
+                    class="social-icon"
                 />
               </div>
             </a>
             <a href="https://t.me/TheTimickRus" target="_blank">
-              <div class="social-list">
+              <div class="social-circle">
                 <AppIcon
-                  name="telegram"
-                  class="social-icon"
+                    name="telegram"
+                    class="social-icon"
                 />
               </div>
             </a>
             <a href="https://soundcloud.com/thetimickrus" target="_blank">
-              <div class="social-list">
+              <div class="social-circle">
                 <AppIcon
-                  name="soundcloud"
-                  class="social-icon"
+                    name="soundcloud"
+                    class="social-icon"
                 />
               </div>
             </a>
@@ -88,13 +88,9 @@
 <script>
 
 import AppIcon from "./AppIcon.vue";
+
 export default {
   components: {AppIcon},
   name: "MainPage"
 }
 </script>
-
-<style scoped>
-
-
-</style>
